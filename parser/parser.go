dce78f2c1a333f9117acae0f0e7711482634418d
Krystian Jarmicki <jarmicki@gmail.com>
2023-05-02 20:03:29 +0200

    parsing return statements

--- parser/parser.go
+++ parser/parser.go
@@ -61,6 +61,8 @@ func (p *Parser) parseStatement() ast.Statement {
 	switch p.curToken.Type {
 	case token.LET:
 		return p.parseLetStatement()
+	case token.RETURN:
+		return p.parseReturnStatement()
 	default:
 		return nil
 	}
@@ -75,6 +77,17 @@ func (p *Parser) parseLetStatement() *ast.LetStatement {
 	if !p.expectPeek(token.ASSIGN) {
 		return nil
 	}
+
+	// TODO: skipping expressions until semicolon
+	for !p.curTokenIs(token.SEMICOLON) {
+		p.nextToken()
+	}
+	return stmt
+}
+
+func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
+	stmt := &ast.ReturnStatement{Token: p.curToken}
+	p.nextToken()
 
 	// TODO: skipping expressions until semicolon
 	for !p.curTokenIs(token.SEMICOLON) {
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -17,15 +17,33 @@ func TestLetStatements(t *testing.T) {
 
 	l := lexer.New(input)
 	p := New(l)
-
 	program := p.ParseProgram()
+
 	checkParserErrors(t, p)
 	assert.NotNil(t, program, "ParseProgram() returned nil")
 	assert.Equal(t, len(program.Statements), 3, "program.Statements does not contain 3 statements")
-
 	testLetStatement(t, program.Statements[0], "x")
 	testLetStatement(t, program.Statements[1], "y")
 	testLetStatement(t, program.Statements[2], "foobar")
+}
+
+func TestReturnStatement(t *testing.T) {
+	input := `
+		return 5;
+		return 10;
+		return 993322;
+	`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+	checkParserErrors(t, p)
+	assert.NotNil(t, program, "ParseProgram() returned nil")
+	assert.Equal(t, len(program.Statements), 3, "program.Statements does not contain 3 statements")
+	testReturnStatement(t, program.Statements[0], "5")
+	testReturnStatement(t, program.Statements[0], "10")
+	testReturnStatement(t, program.Statements[0], "993322")
 }
 
 func testLetStatement(t *testing.T, s ast.Statement, name string) {
@@ -35,6 +53,13 @@ func testLetStatement(t *testing.T, s ast.Statement, name string) {
 	assert.True(t, ok)
 	assert.Equal(t, name, letStmt.Name.Value)
 	assert.Equal(t, name, letStmt.Name.TokenLiteral())
+}
+
+func testReturnStatement(t *testing.T, s ast.Statement, name string) {
+	t.Helper()
+	assert.Equal(t, s.TokenLiteral(), "return")
+	_, ok := s.(*ast.ReturnStatement)
+	assert.True(t, ok)
 }
 
 func checkParserErrors(t *testing.T, p *Parser) {
